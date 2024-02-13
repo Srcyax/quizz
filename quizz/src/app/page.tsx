a3140cf9698ae8658c0f4d9850c691ac8be465b0
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-13 01:04:17 -0300

    a

--- quizz/src/app/page.tsx
+++ quizz/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 className="text-4xl mb-24">Welcome to the Quizz!</h1>
           <div className="bg-black dark:bg-zinc-900 rounded-md group flex flex-col justify-center hover:p-2 transition-all duration-200">
               <h1 className="text-center text-white my-2 group-hover:text-lg transition-all duration-200">What is your <strong>nickname</strong>?</h1>
-              <input id="name" className="bg-white text-black rounded-sm w-64 group-hover:w-60 h-6 m-5 transition-all" type="text" required placeholder="Quizz User" />
+              <input id="name" className="bg-white text-black p-5 rounded-sm w-64 group-hover:w-60 h-6 m-5 transition-all" type="text" required placeholder="Quizz User" />
           </div>
 
           <Link className="flex justify-center" href="/hub">
