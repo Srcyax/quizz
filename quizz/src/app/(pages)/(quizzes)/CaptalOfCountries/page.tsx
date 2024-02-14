f746e65dcd00ae675f3fabbadfb68eb44bf6ad11
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-14 00:16:12 -0300

    Update page.tsx

--- quizz/src/app/(pages)/(quizzes)/CaptalOfCountries/page.tsx
+++ quizz/src/app/(pages)/(quizzes)/CaptalOfCountries/page.tsx
@@ -3,29 +3,27 @@
 import CapitalsQuestion from "./question"
 
 export default function Capitals(){
-    const question = [
+    const questions = [
         {
             question: "Qual a capital do Brasil?",
-            answers: ["Brasília", "São Paulo", "Carlópolis"],
+            answers: ["Brasília", "São Paulo", "Paraná", "Goiana"],
             correctAnswer: "Brasília"
         },
         {
-            question: "Qual a capital do Caralho?",
-            answers: ["Brasília", "São Paulo", "Carlópolis"],
-            correctAnswer: "Brasília"
+            question: "Qual a capital do Japão",
+            answers: ["Aomori", "Miyagi", "Tóquio", "Kanagawa"],
+            correctAnswer: "Tóquio"
         },
         {
             question: "Qual a capital do Peru?",
-            answers: ["Brasília", "São Paulo", "Carlópolis"],
-            correctAnswer: "Brasília"
+            answers: ["Huánuco", "Moquegua", "Lima", "Apurímac"],
+            correctAnswer: "Lima"
         }
     ]
 
     return (
-        <main className="flex flex-col justify-center items-center">
-            
-            <CapitalsQuestion questions={question}/>
-            
+        <main className="flex flex-col justify-center items-center">      
+            <CapitalsQuestion questions={questions}/>      
         </main>
     )
 }
