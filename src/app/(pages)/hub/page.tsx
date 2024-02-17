c1fc45f0c25d8f06daa6453b50f131695cd5b9e7
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-17 12:14:42 -0300

    Update page.tsx

--- src/app/(pages)/hub/page.tsx
+++ src/app/(pages)/hub/page.tsx
@@ -17,7 +17,7 @@ export default function Hub(){
             <h1 className="text-2xl text-center text-zinc-50 rounded-lg p-5 transition 1s bg-black dark:bg-zinc-900 mb-20">Hello, <strong className="text-cyan-500 transition-colors duration-200">{name !== "" ? name : "Quizz User"}</strong></h1>
             <div>
                 <h1 className="text-zinc-100 text-center m-5">Quizzes</h1>
-                <div className="grid grid-cols-3 grid-rows-none place-items-center gap-10 m-10">
+                <div className="grid grid-cols-3 max-[552px]:grid-cols-1 max-[854px]:grid-cols-2 grid-rows-none place-items-center gap-10 m-10">
                     <QuizzCard image="https://cdn-icons-png.flaticon.com/512/854/854878.png"
                     title="Capitals of countries" quizzPage="/CaptalOfCountries"/>
                     <QuizzCard image="https://cdn-icons-png.flaticon.com/512/746/746960.png"
